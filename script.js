0ed2f98ee1c53ccee0f919bb73a0b6d42f90d9d5
llya301a <andrelukaillya@gmail.com>
2023-08-17 23:44:47 +0300

    added homework 3.3

---
+++ script.js
@@ -0,0 +1,3 @@
+let number1 = Number(prompt("Write first number"));
+let number2 = Number(prompt("Write second number"));
+alert("Додавання: "+(number1+number2)+". Віднімання: "+(number1-number2)+". Множення: "+(number1*number2)+". Ділення: "+(number1/number2)+".");
