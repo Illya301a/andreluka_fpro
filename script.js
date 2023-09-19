c223a25abeaba8a7d4328ce821042d98b390d5d4
llya301a <andrelukaillya@gmail.com>
2023-09-20 00:06:53 +0300

    added homework 22

---
+++ script.js
@@ -0,0 +1,10 @@
+//Створити скрипт який має визначити середнє арифметичне трьох чисел з насутпними умовами:
+//
+// отримати від користувача через три prompt три числа
+// показати через alert середнє арифметичне цих чисел
+
+let first = +prompt("Enter first number")
+let second = +prompt("Enter second number")
+let third = +prompt("Enter third number")
+
+alert("Середнє арифметичне: "+((first+second+third)/3))
