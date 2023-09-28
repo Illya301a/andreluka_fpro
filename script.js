b1a292202bb8b3bd4c81b3d11831060c8ca6beec
llya301a <andrelukaillya@gmail.com>
2023-09-29 01:22:21 +0300

    added homework 26

--- script.js
+++ script.js
@@ -19,10 +19,52 @@ function showProducts(x) {
         productArr[i].style.display = x === "product" + (i + 1) ? "block" : "none"
     }
 }
-let buyButtons = document.querySelectorAll("button")
+let buyButtons = document.querySelectorAll(".buy")
 buyButtons.forEach((button) => {
     button.addEventListener("click", function () {
         alert("Товар успішно куплено!")
-        location.reload()
     });
 });
+function buyProduct(productName, price) {
+    const order = {
+        date: new Date().toLocaleDateString(),
+        price: price,
+        productName: productName
+    }
+    const orders = JSON.parse(localStorage.getItem("orders")) || [];
+    orders.push(order);
+    localStorage.setItem("orders", JSON.stringify(orders));
+}
+
+const showOrdersButton = document.getElementById("showOrdersButton");
+const ordersList = document.getElementById("ordersList");
+const ordersUl = document.getElementById("orders");
+
+showOrdersButton.addEventListener("click", function () {
+    const orders = JSON.parse(localStorage.getItem("orders")) || [];
+
+    if (orders.length > 0) {
+        ordersList.style.display = "block";
+        ordersUl.innerHTML = "";
+        orders.forEach(function (order, index) {
+            const li = document.createElement("li");
+            li.textContent = `Замовлення #${index + 1} - ${order.date}, ${order.productName}, ${order.price} грн`;
+            li.innerHTML += ` <button onclick="removeOrder(${index})">Видалити</button>`;
+            ordersUl.appendChild(li);
+        });
+    } else {
+        alert("У вас немає замовлень.");
+        location.reload()
+    }
+});
+
+function removeOrder(index) {
+    const orders = JSON.parse(localStorage.getItem("orders")) || [];
+
+    if (index >= 0 && index < orders.length) {
+        orders.splice(index, 1);
+        localStorage.setItem("orders", JSON.stringify(orders));
+        alert("Замовлення видалено.");
+        showOrdersButton.click();
+    }
+}
