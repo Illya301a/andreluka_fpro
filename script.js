ac11aff37f29b56f987c0d4c961cef791c20d6ac
llya301a <andrelukaillya@gmail.com>
2023-10-17 03:05:16 +0300

    last fix of homework 34

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
+document.querySelector(".category1").addEventListener("click", () =>{
+    showCategory("category1")
+})
+document.querySelector(".category2").addEventListener("click", () =>{
+    showCategory("category2")
+})
+document.querySelector(".product1").addEventListener("click", () =>{
+    showProducts("product1")
+})
+document.querySelector(".product2").addEventListener("click", () =>{
+    showProducts("product2")
+})
+document.querySelector(".product3").addEventListener("click", () =>{
+    showProducts("product3")
+})
+document.querySelector(".product4").addEventListener("click", () =>{
+    showProducts("product4")
+})
+document.querySelector(".buy1").addEventListener("click", () =>{
+    category(1)
+})
+document.querySelector(".buy2").addEventListener("click", () =>{
+    category(2)
+})
+document.querySelector(".buy3").addEventListener("click", () =>{
+    category(3)
+})
+document.querySelector(".buy4").addEventListener("click", () =>{
+    category(4)
+})
+document.querySelector(".confirm").addEventListener("click", () =>{
+    buyProduct()
+})
+
+const buyButtons = document.querySelectorAll(".buy")
+const showOrdersButton = document.getElementById("showOrdersButton");
+const ordersList = document.getElementById("ordersList");
+const ordersUl = document.getElementById("orders");
+const orderFormContainer = document.getElementById("orderFormContainer");
+const orderForm = document.getElementById("orderForm");
+const errorMessage = document.getElementById("error");
+let productName = null;
+let price = null;
+
 function showCategory(x) {
     const categoryArr = [
         document.querySelector(".products1"),
@@ -7,6 +51,7 @@ function showCategory(x) {
         categoryArr[i].style.display = x === "category" + (i + 1) ? "block" : "none"
     }
 }
+
 function showProducts(x) {
     const productArr = [
         document.querySelector(".info1"),
@@ -19,46 +64,43 @@ function showProducts(x) {
         productArr[i].style.display = x === "product" + (i + 1) ? "block" : "none"
     }
 }
-let buyButtons = document.querySelectorAll(".buy")
+
+
 buyButtons.forEach((button) => {
     button.addEventListener("click", function () {
         alert("Товар успішно куплено!")
     });
 });
-let productName
-let price
+
 function category(x) {
-    switch (x){
-        case 1:{
-            productName = "ПЕС ПАТРОН"
-            price = 1300
-        }
-            break
-        case 2:{
-            productName = "ДЖАВЕЛІН"
-            price = 400
-        }
-            break
-        case 3:{
-            productName = "ХЕРСОНСЬКИЙ КАВУНЧИК"
-            price = 80
-        }
-            break
-        case 4:{
-            productName = "ВОКЗАЛЬНИЙ ЧЕБУРЕК"
-            price = 70
-        }
-            break
+    switch (x) {
+        case 1:
+            productName = "ПЕС ПАТРОН";
+            price = 1300;
+            break;
+        case 2:
+            productName = "ДЖАВЕЛІН";
+            price = 400;
+            break;
+        case 3:
+            productName = "ХЕРСОНСЬКИЙ КАВУНЧИК";
+            price = 80;
+            break;
+        case 4:
+            productName = "ВОКЗАЛЬНИЙ ЧЕБУРЕК";
+            price = 70;
+            break;
         default:
     }
 }
+
 function buyProduct() {
     const name = document.getElementById("name").value;
     const city = document.getElementById("city").value;
     const delivery = document.getElementById("delivery").value;
     const payment = document.getElementById("payment").value;
     const quantity = document.getElementById("quantity").value;
-    const comment = document.getElementById("comment").value
+    const comment = document.getElementById("comment").value;
     const order = {
         date: new Date().toLocaleDateString(),
         name: name,
@@ -69,15 +111,16 @@ function buyProduct() {
         productName: productName,
         price: price,
         comment: comment
-    }
+    };
+    if (name !== "" && city !== "" && delivery !== "" && payment !== "" && quantity !== ""){
         const orders = JSON.parse(localStorage.getItem("orders")) || [];
         orders.push(order);
         localStorage.setItem("orders", JSON.stringify(orders));
+    }
+    else{
+        alert("Введіть усі дані!")
+    }
 }
-
-const showOrdersButton = document.getElementById("showOrdersButton");
-const ordersList = document.getElementById("ordersList");
-const ordersUl = document.getElementById("orders");
 
 showOrdersButton.addEventListener("click", function () {
     const orders = JSON.parse(localStorage.getItem("orders")) || [];
@@ -115,10 +158,6 @@ function removeOrder(index) {
         showOrdersButton.click();
     }
 }
-
-let orderFormContainer = document.getElementById("orderFormContainer");
-let orderForm = document.getElementById("orderForm");
-let errorMessage = document.getElementById("error");
 
 buyButtons.forEach((button) => {
     button.addEventListener("click", function () {
