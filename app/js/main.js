15d0248da7fe1d2007477d54448c2d360fafc9f9
llya301a <andrelukaillya@gmail.com>
2023-10-25 22:48:42 +0300

    added homework 38

---
+++ app/js/main.js
@@ -0,0 +1,14 @@
+function add(a, b) {
+    return a+b;
+}
+function divide(a, b) {
+    return a / b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+console.log(add(2, 4))
+console.log(divide(2, 4))
+console.log(multiply(2, 4))
