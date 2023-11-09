52bbfc364ed330795482d4dc65941245315b0878
llya301a <andrelukaillya@gmail.com>
2023-11-09 10:56:16 +0200

    fixed homework 41

--- my-app/src/components/SmileysVoter/SmileysVoter.js
+++ my-app/src/components/SmileysVoter/SmileysVoter.js
@@ -1,50 +1,56 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import "./smileys-voter.css";
 
-export default function SmileysVoter() {
-    const [smileys, setSmileys] = useState([
-        { id: 1, symbol: "😊", votes: 0 },
-        { id: 2, symbol: "😄", votes: 0 },
-        { id: 3, symbol: "😍", votes: 0 },
-        { id: 4, symbol: "😎", votes: 0 },
-        { id: 5, symbol: "🥳", votes: 0 },
-    ]);
+export default class SmileysVoter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            smileys: [
+                { id: 1, symbol: "😊", votes: 0 },
+                { id: 2, symbol: "😄", votes: 0 },
+                { id: 3, symbol: "😍", votes: 0 },
+                { id: 4, symbol: "😎", votes: 0 },
+                { id: 5, symbol: "🥳", votes: 0 },
+            ],
+            showWinner: false,
+            winner: null,
+        };
+    }
 
-    const [showWinner, setShowWinner] = useState(false);
-    const [winner, setWinner] = useState(null);
-
-    const SmileyClick = (id) => {
-        const updatedSmileys = smileys.map((smiley) =>
+    smileyClick = (id) => {
+        const updatedSmileys = this.state.smileys.map((smiley) =>
             smiley.id === id ? { ...smiley, votes: smiley.votes + 1 } : smiley
         );
-        setSmileys(updatedSmileys);
+        this.setState({ smileys: updatedSmileys });
     };
 
-    const ShowResultsClick = () => {
-        const winningSmiley = smileys.reduce((prev, current) =>
+    showResultsClick = () => {
+        const winningSmiley = this.state.smileys.reduce((prev, current) =>
             prev.votes > current.votes ? prev : current
         );
-        setWinner(winningSmiley.symbol);
-        setShowWinner(true);
+        this.setState({ winner: winningSmiley.symbol, showWinner: true });
     };
 
-    return (
-        <div>
-            <div className="smiley-container">
-                {smileys.map((smiley) => (
-                    <div
-                        key={smiley.id}
-                        className="smiley"
-                        onClick={() => SmileyClick(smiley.id)}
-                    >
-                        {smiley.symbol} - {smiley.votes}
-                    </div>
-                ))}
+    render() {
+        const { smileys, showWinner, winner } = this.state;
+        return (
+            <div>
+                <div className="smiley-container">
+                    {smileys.map((smiley) => (
+                        <div
+                            key={smiley.id}
+                            className="smiley"
+                            onClick={() => this.smileyClick(smiley.id)}
+                        >
+                            {smiley.symbol} - {smiley.votes}
+                        </div>
+                    ))}
+                </div>
+                <button className="show-results-button" onClick={this.showResultsClick}>
+                    Show Results
+                </button>
+                {showWinner && <div className="winner-text">Переможець: {winner}</div>}
             </div>
-            <button className="show-results-button" onClick={ShowResultsClick}>
-                Show Results
-            </button>
-            {showWinner && <div className="winner-text">Переможець: {winner}</div>}
-        </div>
-    );
+        );
+    }
 }
