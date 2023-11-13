456b3230acca417108f7089df4170a3387283903
llya301a <andrelukaillya@gmail.com>
2023-11-13 19:16:20 +0200

    added homework 42

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import TodoList from "./components/TodoList/TodoList";
 
 function App() {
   return (
     <>
+      <TodoList />
     </>
   );
 }
---
+++ my-app/src/components/TodoList/TodoList.js
@@ -0,0 +1,91 @@
+import React from 'react';
+
+import "./todo-list.css"
+import refresh from "./refresh.png"
+
+export default class TodoList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos: [
+                { text: 'Wake up!', completed: true },
+                { text: 'Breakfast', completed: false },
+                { text: 'Brush teeth', completed: false },
+            ],
+            newTodo: '',
+        };
+    }
+
+    handleTodoToggle = (index) => {
+        const updatedTodos = [...this.state.todos];
+        updatedTodos[index].completed = !updatedTodos[index].completed;
+        this.setState({ todos: updatedTodos });
+    };
+
+    handleAddTodo = () => {
+        if (this.state.newTodo.trim() !== '') {
+            this.setState({
+                todos: [...this.state.todos, { text: this.state.newTodo, completed: false }],
+                newTodo: '',
+            });
+        }
+    };
+
+
+    handleDeleteTodo = (index) => { //не работает
+        this.state.todos[index] = {}
+    };
+
+    handleRefreshPage = () => {
+        this.setState({ rotating: true });
+        setTimeout(() => {
+            this.setState({ rotating: false });
+            window.location.reload();
+        }, 500);
+    };
+
+    handleKeyPress = (e) => {
+        if (e.key === "Enter") {
+            this.handleAddTodo();
+        }
+    }
+
+    render() {
+        return (
+            <div className="todo-list-container">
+                <img
+                    src={refresh}
+                    className={`refresh ${this.state.rotating ? 'rotate' : ''}`}
+                    alt="refresh"
+                    onClick={this.handleRefreshPage}
+                />
+                <h1>Todo List</h1>
+                <ul className="todo-list">
+                    {this.state.todos.map((todo, index) => (
+                        <li
+                            key={index}
+                            className={`todo-item ${todo.completed ? 'completed' : ''}`}
+                            onClick={() => this.handleTodoToggle(index)}
+                        >
+                            {todo.text}
+                            <span className="delete-button" onClick={() => this.handleDeleteTodo(index)}> X </span>
+                        </li>
+                    ))}
+                </ul>
+                <div className="add-todo-container">
+                    <input
+                        type="text"
+                        value={this.state.newTodo}
+                        onChange={(e) => this.setState({ newTodo: e.target.value })}
+                        onKeyPress={this.handleKeyPress}
+                        placeholder="Add a new todo"
+                        className="todo-input"
+                    />
+                    <button onClick={this.handleAddTodo} className="add-button">
+                        Add Todo
+                    </button>
+                </div>
+            </div>
+        );
+    }
+}
